a242b03571b5fd6e3f292847deec5d1cb3d9a4eb
20-Mayliza-Kustina <mayliza.tkjsmeksa25@gmail.com>
2026-08-31 23:31:17 +0700

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,21 @@
+while True:
+    import fungsi
+    
+    print("Menu Modul")
+    print("1.ganjil genap\n2.huruf vokal\n3.huruf konsonan\n4. merubah capslock")
+    print()
+    a = int(input("No = "))
+    if a == 1:
+        print(fungsi.cek_ganjil_genap(print("ganjil genap /nMasukkan angka = ")))
+    if a == 2:
+        print(fungsi.cek_huruf_vokal(print("huruf vokal /nMasukkan huruf = ")))
+    if a == 3:
+        print(fungsi.cek_huruf_konsonan(print("huruf konsonan /nMasukkan huruf = ")))
+    if a == 4:
+        print(fungsi.cek_merubah_capslock(print("merubah capslock /nMasukkan teks = ")))
+        
+              
+    
+     
+
+
