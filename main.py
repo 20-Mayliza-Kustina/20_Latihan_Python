db450f9bd8bacbca5bc813febc72d1a6835178be
20-Mayliza-Kustina <mayliza.tkjsmeksa25@gmail.com>
2026-08-31 23:40:24 +0700

    modul

--- main.py
+++ main.py
@@ -50,3 +50,24 @@ def cek_merubah_capslock(teks):
                 print(teks.lower())
             else:
                 print(teks.upper())
+                
+while True:
+    import fungsi
+    
+    print("Menu Modul")
+    print("1.ganjil genap\n2.huruf vokal\n3.huruf konsonan\n4. merubah capslock")
+    print()
+    a = int(input("No = "))
+    if a == 1:
+        print(fungsi.cek_ganjil_genap(print("ganjil genap /nMasukkan angka = ")))
+    if a == 2:
+        print(fungsi.cek_huruf_vokal(print("huruf vokal /nMasukkan huruf = ")))
+    if a == 3:
+        print(fungsi.cek_huruf_konsonan(print("huruf konsonan /nMasukkan huruf = ")))
+    if a == 4:
+        print(fungsi.cek_merubah_capslock(print("merubah capslock /nMasukkan teks = ")))
+    pilihan = input("terus atau berhenti? (terus/berhenti): ")
+ 
+    if pilihan == "berhenti":
+        print("program selesai, terimakasih")
+        break
