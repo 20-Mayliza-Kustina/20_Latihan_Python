c1da6d8d3ffcec7fc63623a53c4b13a6dd935133
20-Mayliza-Kustina <mayliza.tkjsmeksa25@gmail.com>
2026-09-11 09:22:11 +0700

    tambah database

--- fungsi.py
+++ fungsi.py
@@ -1,27 +1,49 @@
-print("========================== FUNGSI OLAH KATA ================================")
+import sqlite3
 
+def register_user(username, password):
+    try:
+        conn = sqlite3.connect("database_fungsi.db")
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+        conn.commit()
+        conn.close()
+        return "Registrasi berhasil!"
+    except sqlite3.IntegrityError:
+        return "Username kamu sudah terdaftar!"
+
+def login_user(username, password):
+    conn = sqlite3.connect("database_fungsi.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
+    user = cursor.fetchone()
+    conn.close()
+    
+    if user is not None:
+        return True  
+    else:
+        return False
 def cek_ganjil_genap(angka):
             # Menggunakan pembagian bulat (//) dan perkalian (*)
             if (angka // 2) * 2 == angka:
-                print("GENAP")
+                return "GENAP"
             else:
-                print("GANJIL")
-                
+                return "GANJIL"
+
 def cek_huruf_vokal(huruf):
             if huruf.lower() in ['a', 'e', 'i', 'o', 'u']:
-                print("huruf vokal")
+                return "huruf vokal"
             else:
-                print("bukan huruf vokal")
-                
+                return "bukan huruf vokal"
+
 def cek_huruf_konsonan(huruf):
             if huruf.lower() not in ['a', 'e', 'i', 'o', 'u'] and huruf.isalpha():
-                print("huruf konsonan")
+                return "huruf konsonan"
             else:
-                print("bukan huruf konsonan")
+                return "bukan huruf konsonan"
 
 def cek_merubah_capslock(teks):
             if teks.isupper():
-                print(teks.lower())
+                return teks.lower()
             else:
-                print(teks.upper())
+                return teks.upper()
                 
